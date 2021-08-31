58d7c021b4eef6ac90636a56d803e6d0725d08e2
Trpquo <trpimir.jezic@gmail.com>
2021-09-01 00:22:04 +0200

    Dodani svi engleski tekstovi i potpisi

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -9,8 +9,8 @@
 
 <footer>
     <nav>
-        <button on:click={ setLanguage("hr") } >HR</button>
-        <button on:click={ setLanguage("en") } >EN</button>
+        <button on:click={ setLanguage("hr") } class:active={ $language !== 'en' } >HR</button>
+        <button on:click={ setLanguage("en") } class:active={ $language === 'en' }>EN</button>
     </nav>
 </footer>
 
@@ -20,5 +20,25 @@
         background-color: var(--accent-color2);
         padding: 0 var(--default-padding);
         color: var(--text-color-negative);
+        font-family: var(--main-smallcaps);
+    }
+    nav {
+        padding: 1rem;
+        text-align: right;
+    }
+
+    button {
+        background-color: var(--accent-color2);
+        color: var(--text-color-negative);
+        margin-left: .5rem;
+        transition: .5s;
+    }
+    button.active {
+        border-color: var(--accent-color3)
+    }
+    button:hover:not( .active ) {
+        background-color: var(--text-color-negative);
+        color: var(--accent-color2);
+        cursor: pointer;
     }
 </style>
